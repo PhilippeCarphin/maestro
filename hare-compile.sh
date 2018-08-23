262f22df0bb1dfb97b3f9185409c74d1404040db
Stuart Spence <stuart.spence@canada.ca>
2018-08-23 14:33:04 +0000

    removed all from maestro-core make, used parent make for tcl instead of two

--- hare-compile.sh
+++ hare-compile.sh
@@ -4,7 +4,7 @@ MAESTRO_TCL="$PWD/maestro-tcl"
 
 ssh hare "
 
-cd $MAESTRO_CORE/src
+
 
 # Load gnu compiler instead of default intel:
 module switch PrgEnv-intel/5.2.82 PrgEnv-gnu
@@ -12,12 +12,7 @@ module switch PrgEnv-intel/5.2.82 PrgEnv-gnu
 # Put a generic environment tag instead of chipset specific name
 export ORDENV_PLAT=sles-11-amd64-64
 
-# Compile with dynamic libs:
-make clean
-make
-
-# Create ssm package:
-cd $MAESTRO_CORE/ssm
+cd $MAESTRO_CORE
 make clean
 make
 
