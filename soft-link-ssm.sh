4591fe5c8dd23e7ef8e603aa6822165d0a109717
Stuart Spence <stuart.spence@canada.ca>
2018-08-23 20:31:33 +0000

    added safe create ssm to soft link script

--- soft-link-ssm.sh
+++ soft-link-ssm.sh
@@ -1,5 +1,6 @@
 # This script searches for all ssm files in child directories and creates a soft link in the ssm folder.
 
+mkdir -p ssm
 for path in `find . -name *.ssm -exec readlink -f {} \;` ; do
 		filename=`basename $path`
 		rm -f ssm/$filename
