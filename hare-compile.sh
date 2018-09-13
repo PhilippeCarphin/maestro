552ef967400c825a86050abb57df4de2cc52a272
Stuart Spence <stuart.spence@canada.ca>
2018-09-13 21:19:21 +0000

    fixed hare compile make path

--- hare-compile.sh
+++ hare-compile.sh
@@ -7,10 +7,7 @@ module switch PrgEnv-intel/5.2.82 PrgEnv-gnu
 # Put a generic environment tag instead of chipset specific name
 export ORDENV_PLAT=sles-11-amd64-64
 
-cd $PWD/src/core
-make || exit 1
-
-cd $PWD/src/tcl
+cd $PWD
 make || exit 1
 
 " | ssh hare bash --login
