0c33ee1aa386e2bb1a2d6c47127128b932267dfc
Stuart Spence <stuart.spence@canada.ca>
2018-09-18 20:06:23 +0000

    removed remaining env(SEQ_MANAGER_BIN)/../ from tcls

--- src/xm/lib/common/dkffont.tcl
+++ src/xm/lib/common/dkffont.tcl
@@ -631,7 +631,7 @@ namespace eval ::dkfFontSel {
     # based on the current filename.
     proc 'load_resources {filebase} {
         global env
-        set dirbase $env(SEQ_MANAGER_BIN)/../etc/config/
+        set dirbase $env(SEQ_PACKAGE_HOME)/src/xflow/etc/config/
 	set filebase [file rootname $filebase]
         set filebase [file tail $filebase]
 	option readfile ${dirbase}${filebase}.ad widgetDefault
