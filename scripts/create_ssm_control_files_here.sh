415dedee07c040dfe4fdd2ee747b438ff939af24
Stuart Spence <stuart.spence@canada.ca>
2018-09-21 16:22:04 +0000

    added dynamic fetch url to ssm control

--- scripts/create_ssm_control_files_here.sh
+++ scripts/create_ssm_control_files_here.sh
@@ -1,7 +1,12 @@
 # This script uses variables in the environment to create the control and control.json files for SSM packages.
 # It creates "control" and "control.json" in the current folder.
+# If given, the first argument of the script is used as the 'git fetch url' to describe where to get the repo.
 
 DESCRIPTION="Maestro is a suite of tools which organize, visualize, schedule, validate, and submit tasks to computer systems."
+if [ ! -z "$1" ]; then
+		DESCRIPTION="$DESCRIPTION You can get an up to date git repo here: $1"
+fi
+
 TITLE="maestro"
 
 echo "Package: ${TITLE}
