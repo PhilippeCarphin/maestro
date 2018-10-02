bffdfce2cb43414801ea4218ae02abdfbb90e755
Stuart Spence <stuart.spence@canada.ca>
2018-10-02 21:13:49 +0000

    added files missing in combined repo, but present in first commit of upkeep2018 repo

--- xflow/lib/SharedFlowNode.tcl
+++ xflow/lib/SharedFlowNode.tcl
@@ -218,7 +218,7 @@ proc SharedFlowNode_searchSubmitNode { exp_path node datestamp submitted_node } 
 proc SharedFlowNode_searchForTask { exp_path flow_node datestamp } {
    set value ""
    if { $flow_node != "" } {
-      if { [string match *task [SharedFlowNode_getGenericAttribute ${exp_path} ${flow_node} ${datestamp} type] ] } {
+      if { [SharedFlowNode_getGenericAttribute ${exp_path} ${flow_node} ${datestamp} type] == "task" } {
          set value $flow_node
       } else {
          set value [SharedFlowNode_searchForTask ${exp_path} [SharedFlowNode_getGenericAttribute ${exp_path} ${flow_node} ${datestamp} submitter] ${datestamp}]
