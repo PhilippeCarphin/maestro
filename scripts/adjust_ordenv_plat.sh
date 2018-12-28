965dcd20461263450cf069d0d99e0ad295707af2
Stuart Spence <stuart.spence@canada.ca>
2018-12-28 21:37:59 +0000

    commented some of the new scripts

--- scripts/adjust_ordenv_plat.sh
+++ scripts/adjust_ordenv_plat.sh
@@ -3,6 +3,7 @@
 # This script prints the value of ORDENV_PLAT
 # However on some platforms, an adjusted value is printed instead.
 
+# find the full path which contains this script file, no matter where it is called from.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [[ "$(bash ${SCRIPT_DIR}/is_platform_xc40.sh )" = "true" ]] ; then
