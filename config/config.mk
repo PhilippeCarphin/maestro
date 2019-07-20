c5150b8d0066feb3457762aa87c3bd07fb470d9b
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-07-20 00:53:29 -0400

    Changed paths for new system layout

--- config/config.mk
+++ config/config.mk
@@ -12,13 +12,13 @@ BINDIR=$(SWDEST)/bin
 
 
 # libxml2 stuff
-XML_LIB_DIR=/usr/local/Cellar/libxml2/2.9.9_2/lib
-XML_INCLUDE_DIR = /usr/local/Cellar/libxml2/2.9.9_2/include/libxml2
+XML_LIB_DIR=/usr/local/opt/libxml2/
+XML_INCLUDE_DIR = /usr/local/opt/libxml2/include/libxml2
 # XML_INCLUDE_DIR = /usr/include/libxml2
 # XML_INCLUDE_DIR = /usr/local/Cellar/libxml2/2.9.7/include/libxml2
 
-OPENSSL_INCLUDE_DIR=/usr/local/Cellar/openssl/1.0.2q/include/
-OPENSSL_LIB_DIR=/usr/local/Cellar/openssl/1.0.2q/lib/
+OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include/
+OPENSSL_LIB_DIR=/usr/local/opt/openssl/lib/
 
 
 # platform specific definition
