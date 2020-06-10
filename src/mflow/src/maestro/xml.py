82508c24b61a2da385b87f43fed9bd56075d6a81
Stuart Spence <stuart.spence@canada.ca>
2020-06-10 18:42:13 +0000

    better logging, gitignore tmp test file

--- src/mflow/src/maestro/xml.py
+++ src/mflow/src/maestro/xml.py
@@ -423,8 +423,6 @@ def get_combined_flow_from_text_list(xml_datas,verbose=False):
                 
             made_progress=True
     
-    for m in incomplete_modules:
-        pprint(m)
     if incomplete_modules:
         logger.error("combine flow XML failed to resolve all modules containing empty module children.")
     
