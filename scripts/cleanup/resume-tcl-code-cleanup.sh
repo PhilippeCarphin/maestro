10a1b74636be5d31e622ef62c6c94f0d7cee19af
Stuart Spence <stuart.a.spence@gmail.com>
2020-06-25 15:06:18 -0400

    removed cleanup folder, moved out remove_commented_code.py to scripts

--- scripts/cleanup/resume-tcl-code-cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for path in $(cat tcl-files) ; do
-	echo "Starting commented code stripping for:
-	$path"
-	./remove_commented_code.py tcl /home/zulban/eccc/ziggurat/maestro/$path
-done
