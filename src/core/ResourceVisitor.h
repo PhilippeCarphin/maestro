5b81f530b1cd2a1a1c393e81a8eae1f2da659b09
Stuart Spence <stuart.a.spence@gmail.com>
2020-06-25 14:32:05 -0400

    remove legal header from c files, moving to license file

--- src/core/ResourceVisitor.h
+++ src/core/ResourceVisitor.h
@@ -1,23 +1,6 @@
 /* ResourceVisitor.c - Parses data from resource XML file of a node into the
  * nodeDataPtr.
- * Copyright (C) 2011-2015  Operations division of the Canadian Meteorological Centre
- *                          Environment Canada
- *
- * Maestro is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation,
- * version 2.1 of the License.
- *
- * Maestro is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
+*/
 
 #ifndef _RESOURCE_VISITOR_H_
 #define _RESOURCE_VISITOR_H_
