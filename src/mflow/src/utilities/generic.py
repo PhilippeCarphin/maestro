33d99fdab574bc91d8476f28cf856d6418059200
Stuart Spence <stuart.a.spence@gmail.com>
2020-07-03 11:34:39 -0400

    tests pass with smarter node log parsing

--- src/mflow/src/utilities/generic.py
+++ src/mflow/src/utilities/generic.py
@@ -2,7 +2,9 @@
 The contents of this script are generally useful and can be copied, without modification, to other python projects.
 """
 
-import os, gzip, os.path
+import os
+import gzip
+import os.path
 from utilities.colors import print_green
 from utilities.path import get_matching_paths_recursively
 
@@ -16,6 +18,29 @@ def get_change_time(path):
     except OSError:
         "this may occur because of infinitely nested soft links"
         return "<OSError>"
+
+def cache(function):
+    """
+    later versions of Python have 'functools.cache'
+    """
+    memo = {}
+    def wrapper(*args):
+        if args in memo:
+            return memo[args]
+        else:
+            rv = function(*args)
+            memo[args] = rv
+            return rv
+    return wrapper
+
+def strip_comments_from_text(text):
+    """
+    Return the text content of this string, minus any lines that are
+    comments, like '#' in bash.
+    """
+    lines=text.split("\n")    
+    lines=[i for i in lines if not i.strip().startswith("#")]
+    return "\n".join(lines)
 
 def insert_into_dictionary(a,b):
     """
@@ -126,6 +151,44 @@ def safe_get_lines(path):
             pass
         
     return []
+
+def reverse_readlines(path, buf_size=8192, encoding="utf-8"):
+    """
+    A generator that returns the lines of a file in reverse order.
+    Effective for reading just the end of big files.
+    
+    From:
+        https://stackoverflow.com/a/23646049
+    """
+    with open(path,encoding=encoding) as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            remaining_size -= buf_size
+            lines = buffer.split('\n')
+            # The first line of the buffer is probably not a complete line so
+            # we'll save it and append it to the last line of the next buffer
+            # we read
+            if segment is not None:
+                # If the previous chunk starts right from the beginning of line
+                # do not concat the segment to the last line of new chunk.
+                # Instead, yield the segment first 
+                if buffer[-1] != '\n':
+                    lines[-1] += segment
+                else:
+                    yield segment
+            segment = lines[0]
+            for index in range(len(lines) - 1, 0, -1):
+                if lines[index]:
+                    yield lines[index]
+        # Don't yield None if the file was empty
+        if segment is not None:
+            yield segment
 
 def safe_write(path,data,verbose=True):
     "Writes this data to path, creates any necessary folders."
