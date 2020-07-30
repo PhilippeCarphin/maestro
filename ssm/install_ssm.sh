9a117228dc82e0edc6f1e5a621fb24ada9d8c261
Stuart Spence <stuart.spence@canada.ca>
2020-07-30 18:06:07 +0000

    install_ssm.sh less verbose, and says abort and explains if no SSM of that version is found

--- ssm/install_ssm.sh
+++ ssm/install_ssm.sh
@@ -40,28 +40,35 @@ done
 
 echo "$USAGE"
 
-set -x
-
 # Constants
+set -x
 PROJECT_PATH=$(git rev-parse --show-toplevel)
+PROJECT_SSM_PATH=$PROJECT_PATH/ssm
 NAME=$(basename $PROJECT_PATH)
 INSTALLED_MAESTRO_PATH=$SSM_ROOT/$NAME
 SSM_DOMAIN_PATH=$INSTALLED_MAESTRO_PATH/$VERSION
 # If we find builds for these platforms, make them into ssm packages.
 PLATFORMS=$(cat $PROJECT_PATH/ssm/supported_platforms)
+set +x
 
 # Remove previous
 if [[ $DELETE_ALL_SSM = "true" ]]; then
+	set -x
     	rm -rf $INSTALLED_MAESTRO_PATH
+	set +x
 fi
 if [[ $REINSTALL = "true" ]]; then
+	set -x
 	rm -rf $SSM_DOMAIN_PATH/*${ORDENV_PLAT}*
+	set +x
 	SSM_PACKAGES=""
 	for platform in $PLATFORMS ; do
 		SSM_PACKAGE=ssm/maestro_${VERSION}_${platform}.ssm
 		if [[ -f $SSM_PACKAGE ]]; then
+			set -x
 			ssm unpublish -p maestro_${VERSION}_${platform} -d $SSM_DOMAIN_PATH
 			ssm uninstall -p maestro_${VERSION}_${platform} -d $SSM_DOMAIN_PATH
+			set +x
 		fi
 	done
 fi
@@ -69,7 +76,9 @@ fi
 # Install new
 
 if [[ ! -d $SSM_DOMAIN_PATH ]] ; then
+	set -x
 	ssm created -d $SSM_DOMAIN_PATH
+	set +x
 fi
 
 cd $PROJECT_PATH
@@ -78,12 +87,28 @@ for platform in $PLATFORMS ; do
 		SSM_PACKAGE=ssm/maestro_${VERSION}_${platform}.ssm
 		if [[ -f $SSM_PACKAGE ]]; then
 				SSM_PACKAGES="${SSM_PACKAGES} ${SSM_PACKAGE}"
+				set -x
 				ssm install -f $SSM_PACKAGE -d $SSM_DOMAIN_PATH
 				ssm publish -p maestro_${VERSION}_${platform} -d $SSM_DOMAIN_PATH -pp $platform 
+				set +x
 		fi
 done
 
-set +x
+if [[ -z "$SSM_PACKAGES" ]] ; then
+	echo "
+Aborted. Did not find any SSM packages to install in '$PROJECT_SSM_PATH'.
+Perhaps version argument '$VERSION' is incorrect?"
+	all_ssm_packages=$(find $PROJECT_SSM_PATH -name "*.ssm" -type f -exec basename {} \;)
+	if [[ -z $all_ssm_packages ]] ; then
+		echo "Found no SSM packages."
+	else
+		echo "SSM packages in '$PROJECT_SSM_PATH':
+
+$all_ssm_packages
+	"
+	fi
+	exit 1
+fi
 
 echo "Installed and published: '$SSM_PACKAGES'"
 
