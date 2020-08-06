61764937e9367ff0f017f61512c484caca3781ba
Stuart Spence <stuart.spence@canada.ca>
2020-08-06 21:06:15 +0000

    combined all versions into one in xflow about

--- src/xflow/lib/About.tcl
+++ src/xflow/lib/About.tcl
@@ -12,35 +12,17 @@ proc About_show { parent } {
 
    ${scrolledW} setwidget ${textW}
 
-   ${textW} tag configure BOLD_TXT -font TkHeadingFont
-   set xflowVersion "unknown"
-   if { [info exists env(SEQ_XFLOW_VERSION)] } {
-      set xflowVersion $env(SEQ_XFLOW_VERSION)
-   }
-   ${textW} insert end "Xflow version ${xflowVersion}" BOLD_TXT
-
-   set utilsVersion "unknown"
-   if { [info exists env(SEQ_UTILS_VERSION)] } {
-      set utilsVersion $env(SEQ_UTILS_VERSION)
-   }
-   ${textW} insert end "\n\nMaestro utilities version ${utilsVersion}" BOLD_TXT
-
-   set seqVersion "unknown"
+   set maestroVersion "unknown"
    if { [info exists env(SEQ_MAESTRO_VERSION)] } {
-      set seqVersion $env(SEQ_MAESTRO_VERSION)
+      set maestroVersion $env(SEQ_MAESTRO_VERSION)
    }
-   ${textW} insert end "\n\nMaestro sequencer version ${seqVersion}" BOLD_TXT
+   ${textW} insert end "\n\nMaestro version: ${maestroVersion}" BOLD_TXT
 
-   ${textW} insert end "\n\nMaestro documentation:\n" BOLD_TXT
+   ${textW} insert end "\n\nMaestro resources and training:\n" BOLD_TXT
 
    # should create an app properties file in next version
    set docUrl "https://wiki.cmc.ec.gc.ca/wiki/Maestro"
 
-   #font create MyUnderlinedFont
-   #set defaultCfg [font configure TkDefaultFont]
-   #eval font configure MyUnderlinedFont ${defaultCfg}
-   #font configure MyUnderlinedFont -underline true
-   #set docLabel [label ${textW}.doc_label -font MyUnderlinedFont -text ${docUrl} -fg blue]
    set docLabel [label ${textW}.doc_label -text ${docUrl} -fg blue]
    ${textW} window create end -window ${docLabel}
 
@@ -48,6 +30,5 @@ proc About_show { parent } {
     
    ${textW} configure -state disabled
    pack ${scrolledW} -fill both -expand yes -padx 5 -pady 5 -ipadx 2 -ipady 2
-   #wm geometry ${topW} =400x200
 }
 
